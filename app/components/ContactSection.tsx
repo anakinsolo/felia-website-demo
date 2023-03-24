4103ff8e19399c175464dda385b3794c247404d9
Tuan Dinh <tuan.dinh@felia.fi>
2023-03-24 23:39:15 +0700

    updated background image

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -5,6 +5,13 @@ export default function ContactSection() {
       <div className='contact-info'>
         <div className='title'>Get in <span className='inner-text'>touch</span></div>
         <div className='title small'>with our <span className='inner-text'>team</span></div>
+        <div className='contact-detail-container'>
+          <div>Email: hello@felia.fi</div>
+          <div>Tel: (+358)50 357 7421</div>
+          <div className='social-media-container'>
+            <a href='https://www.facebook.com/felia.fi/' target='_blank' rel='noopener noreferrer'>Website</a>
+          </div>
+        </div>
       </div>
       <div className='contact-form'>Contact form here</div>
     </div>
