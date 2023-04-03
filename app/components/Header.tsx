e0b5336433040a312903ba1a112a97a977e79197
Tuan Dinh <tuan.dinh@felia.fi>
2023-04-04 03:16:53 +0700

    Updated icon

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,25 +2,29 @@ import { Link } from '@remix-run/react';
 import Button from './Button';
 import SvgLogo from './icons/Logo';
 import stylesheet from '~/assets/styles/components/header.css';
+import React from 'react';
 
 export const links = [
   { rel: 'stylesheet', href: stylesheet },
 ];
 
 export default function Header() {
-  function testFunction(): void {
-    console.log('test');
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  function toggleMenu(): void {
     let x = document.getElementById('menu-nav');
 
     if (x) {
       if (x.className === 'menu') {
         x.className += ' responsive';
+        setIsMenuOpen(true);
       } else {
         x.className = 'menu';
+        setIsMenuOpen(false);
       }
     }
   }
-  
+
   return (
     <header>
       <div className='container header-container'>
@@ -37,7 +41,7 @@ export default function Header() {
         </div>
         <div className='button-groups'>
           <Button label='Contact us' to='#contact-section' className='primary' />
-          <Button label='fa fa-bars' className='icon' isIcon onClick={testFunction}></Button>
+          <Button label={isMenuOpen ? 'fa fa-times' : 'fa fa-bars'} className='icon' isIcon onClick={toggleMenu} />
         </div>
       </div>
     </header>
