f30ead644ae0b8c6b1e9ae3ecac50caea51549c6
Tuan Dinh <tuan.dinh@felia.fi>
2023-04-14 17:49:31 +0700

    Removed publised

--- app/routes/customers.tsx
+++ app/routes/customers.tsx
@@ -16,7 +16,7 @@ export const links: LinksFunction = () => [
 export const loader = async ({ params }: LoaderArgs) => {
   const slug = 'customers';
   let sbParams: ISbStoriesParams = {
-    version: process.env.NODE_ENV !== 'production' ? 'draft' : 'published',
+    version: 'draft',
   };
 
   let { data } = await getStoryblokApi().get(`cdn/stories/${slug}`, sbParams);
@@ -24,38 +24,12 @@ export const loader = async ({ params }: LoaderArgs) => {
 };
 
 export default function Customers() {
-  // const works = [
-  //   {
-  //     id: 1,
-  //     title: 'The Grand Good Riddance website',
-  //     description: 'UI & UX Design  /  E-commerce Development'
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'The Grand Good Riddance website',
-  //     description: 'UI & UX Design  /  E-commerce Development',
-  //     isReversed: 'reversed'
-  //   }
-  // ];
-
   let story = useLoaderData();
   story = useStoryblokState(story);
 
   return (
     <div className='container'>
       <StoryblokComponent blok={story.content} />
-      {/* <section className='portfolio-page'>
-        <div className='title'>Our <span className='inner-text'>works</span></div>
-        <div className='portfolio-list'>
-          {
-            works.map((work) => {
-              return (
-                <PortfolioItem key={work.id} title={work.title} description={work.description} isReversed={work.isReversed ?? ''} />
-              );
-            })
-          }
-        </div>
-      </section> */}
     </div>
   );
 }
